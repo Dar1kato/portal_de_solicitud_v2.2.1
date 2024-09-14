062f2d98929fe169e0c904bd36ebbd4a8ade03ae
Daniel Peña <149815244+Dar1kato@users.noreply.github.com>
2024-09-14 00:43:40 -0600

    Implementación de Imagenes, 14/09/24

--- src/App.js
+++ src/App.js
@@ -18,28 +18,28 @@ function App() {
   //? NOTA: para fines de prueba se utiliza un array con datos. En la práctica sera necesario crear esta variable con datos recogidos de una base de datos externa
   //TODO queda pendiente implementar el query a la base de datos principal donde estan guardados los datos de los productos
   const datos = {
-    producto1: { nombre: 'Laptop', categoria: 'Ingeniería en Sistemas' },
-    producto2: { nombre: 'Multímetro', categoria: 'Electrónica' },
-    producto3: { nombre: 'Calculadora Científica', categoria: 'Química' },
-    producto4: { nombre: 'Impresora 3D', categoria: 'Mecatrónica' },
-    producto5: { nombre: 'Protoboard', categoria: 'Electrónica' },
-    producto6: { nombre: 'Simulador de Circuitos', categoria: 'Ingeniería en Sistemas' },
-    producto7: { nombre: 'Pipetas', categoria: 'Química' },
-    producto8: { nombre: 'Sensor Ultrasónico', categoria: 'Mecatrónica' },
+    producto1: { nombre: 'Estación de Calor', categoria: 'Electrónica', imagenURL: "https://tronfix.com/3959-large_default/estacion-de-calor-quick-861dw.jpg" },
+    producto2: { nombre: 'Multímetro', categoria: 'Electrónica', imagenURL: "https://www.steren.com.mx/media/catalog/product/cache/0236bbabe616ddcff749ccbc14f38bf2/image/200344c8a/multimetro-digital-auto-rango-con-detector-de-voltaje.jpg"},
+    producto3: { nombre: 'Calculadora Científica', categoria: 'Química', imagenURL: "https://www.adosa.com.mx/media/catalog/product/cache/4db89bde2062a3745aaded91f69a9032/0/8/087590_1.jpg" },
+    producto4: { nombre: 'Impresora 3D', categoria: 'Mecatrónica', imagenURL: "https://m.media-amazon.com/images/I/61L4aoIqYOL._AC_UF1000,1000_QL80_.jpg" },
+    producto5: { nombre: 'Protoboard', categoria: 'Electrónica', imagenURL: "https://aelectronics.com.mx/893/protoboard-blanca-de-830-puntos.jpg" },
+    producto6: { nombre: 'Monitor', categoria: 'Ingeniería en Sistemas', imagenURL:"https://download.lenovo.com/km/media/images/PD500388/thinkvision_p24h_20200219032455975.jpg" },
+    producto7: { nombre: 'Pipetas', categoria: 'Química', imagenURL: "https://provinow.com/wp-content/uploads/2022/11/PIPETA-SEROLOGICA-POLIESTIRENO-10ML-NARANJA-ESTERIL-SPL_.jpg" },
+    producto8: { nombre: 'Sensor Ultrasónico', categoria: 'Mecatrónica', imagenURL:"https://www.steren.com.mx/media/catalog/product/cache/0236bbabe616ddcff749ccbc14f38bf2/image/1945948ab/sensor-ultrasonico.jpg" },
     producto9: { nombre: 'Disco Duro Externo', categoria: 'Ingeniería en Sistemas' },
-    producto10: { nombre: 'Osciloscopio', categoria: 'Electrónica' },
+    producto10: { nombre: 'Osciloscopio', categoria: 'Electrónica', imagenURL: "https://www.steren.com.mx/media/catalog/product/cache/0236bbabe616ddcff749ccbc14f38bf2/image/214806594/osciloscopio-de-100-mhz.jpg" },
     producto11: { nombre: 'Kit de Soldadura', categoria: 'Electrónica' },
     producto12: { nombre: 'Cuaderno de Laboratorio', categoria: 'Química' },
     producto13: { nombre: 'Microcontrolador', categoria: 'Mecatrónica' },
     producto14: { nombre: 'Gafas de Protección', categoria: 'Química' },
-    producto15: { nombre: 'Raspberry Pi', categoria: 'Ingeniería en Sistemas' },
+    producto15: { nombre: 'Raspberry Pi', categoria: 'Ingeniería en Sistemas', imagenURL: "https://mm.digikey.com/Volume0/opasdata/d220001/medias/images/764/MFG_RASPBERRY-PI-3-MODEL-B.jpg" },
     producto16: { nombre: 'Regulador de Voltaje', categoria: 'Electrónica' },
     producto17: { nombre: 'Kit de Robótica', categoria: 'Mecatrónica' },
     producto18: { nombre: 'Software de Modelado 3D', categoria: 'Mecatrónica' },
     producto19: { nombre: 'Programa de Simulación Química', categoria: 'Química' },
-    producto20: { nombre: 'Memoria USB', categoria: 'Ingeniería en Sistemas' },
+    producto20: { nombre: 'Memoria USB', categoria: 'Ingeniería en Sistemas', imagenURL: "https://stgulabmexicoprod.blob.core.windows.net/correosclic/0006019_memoria-usb-64-gb.jpeg" },
     producto21: { nombre: 'Servidor en la nube', categoria: 'Ingeniería en Sistemas' },
-    producto22: { nombre: 'Tarjeta de red', categoria: 'Ingeniería en Sistemas' },
+    producto22: { nombre: 'Tarjeta de red', categoria: 'Ingeniería en Sistemas', imagenURL: "https://media.digitalife.com.mx/products/21347/626b26886906b.webp" },
     producto23: { nombre: 'Router', categoria: 'Ingeniería en Sistemas' },
     producto24: { nombre: 'Kit de Arduino', categoria: 'Ingeniería en Sistemas' },
     producto25: { nombre: 'Cable de fibra óptica', categoria: 'Ingeniería enSistemas' },
@@ -50,14 +50,14 @@ function App() {
     producto30: { nombre: 'Fuente de alimentación', categoria: 'Electrónica' },
     producto31: { nombre: 'Matraz Erlenmeyer', categoria: 'Química' },
     producto32: { nombre: 'Bureta', categoria: 'Química' },
-    producto33: { nombre: 'Balanza analítica', categoria: 'Química' },
+    producto33: { nombre: 'Balanza analítica', categoria: 'Química', imagenURL: "https://www.balpesac.com/wp-content/uploads/2016/09/balanzas-analiticas-bt.png" },
     producto34: { nombre: 'Mechero Bunsen', categoria:'Química' },
     producto35: { nombre: 'Kit de reactivos', categoria: 'Química' },
-    producto36: { nombre: 'Motor DC', categoria:'Mecatrónica' },
-    producto37: { nombre: 'Servo motor', categoria:'Mecatrónica' },
-    producto38: { nombre: 'Encoder', categoria:'Mecatrónica' },
+    producto36: { nombre: 'Motor DC', categoria:'Mecatrónica', imagenURL: "https://robtech.com.mx/wp-content/uploads/2022/07/HR0244.jpg" },
+    producto37: { nombre: 'Servo motor', categoria:'Mecatrónica', imagenURL: "https://www.aranacorp.com/wp-content/uploads/towerpro-servo-sg90.jpg" },
+    producto38: { nombre: 'Encoder', categoria:'Mecatrónica', imagenURL: "https://dominion.com.mx/tienda/974-large_default/e40s6-1024-6-l-5-encoder-rotativo-autonics.jpg" },
     producto39: { nombre: 'Sensor de temperatura', categoria:'Mecatrónica' },
-    producto40: { nombre: 'Controlador lógico programable (PLC)', categoria:'Mecatrónica' }
+    producto40: { nombre: 'Controlador lógico programable (PLC)', categoria:'Mecatrónica'}
   };
   
   const nombresData = Object.values(datos).map(prod => prod.nombre);
@@ -148,7 +148,9 @@ function App() {
           <div className="displayItem" key={index}>
             {/* Se declara el uso de la función agregarPedido() al activar el botón */}
             <b>{item.nombre}</b><br />
-            <small>{item.categoria}</small><button className="addButton" onClick={(e) => agregarPedido(e, item)}>+</button> 
+            <small>{item.categoria}</small>
+            <img src={item.imagenURL} />
+            <button className="addButton" onClick={(e) => agregarPedido(e, item)}>+</button> 
           </div>
         ))}
         {filtro.length === 0 && <p className="noMatch">No se encontraron productos</p>}
@@ -182,8 +184,9 @@ function App() {
 
     return (
       <>
-      Filtrar por categoria: 
+      <b>Filtrar por categoria: </b>
       <select
+        className="menuFiltrado"
         value={selectedValue}
         onChange={(e) => setSelectedValue(e.target.value)}
       >
@@ -209,7 +212,7 @@ function App() {
       <form className="buscar_texto" type="search" >
 
         {/* 5.1. Lógica de búsqueda automática */}
-        Buscar:{" "} <input type="text" onChange={(e) => setQuery(e.target.value)}/> 
+        <input placeholder="Buscar..." type="text" onChange={(e) => setQuery(e.target.value)}/> 
 
       </form>
       <br />
